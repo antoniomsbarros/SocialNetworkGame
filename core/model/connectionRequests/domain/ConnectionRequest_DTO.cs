3e9bc4eb30bb9e15b054fbc78b87dca40da75075
Antonio Barros (1200606) <1200606@isep.ipp.pt>
2021-11-10 22:53:49 +0000

    OL41-10 #comment criação para os DTO do Connection e Introduction

---
+++ core/model/connectionRequests/domain/ConnectionRequest_DTO.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace SocialNetwork.core.model.connectionRequests.domain
+{
+    public class ConnectionDto
+    {
+        public Guid Id { get; set; }
+        
+        public string ConnectionRequestStatus { get; set; }
+
+        public string PlayerSender { get; set; }
+        
+        public string PlayerReceiver { get; set; }
+        
+        public string Text { get; set; }
+        
+        public string CreationDate { get; set; }
+        
+    }
+}
---
+++ core/services/IntroductionRequestService.cs
@@ -0,0 +1,18 @@
+﻿using SocialNetwork.core.model.connectionRequests.domain;
+using SocialNetwork.core.model.connectionRequests.repository;
+using SocialNetwork.infrastructure;
+
+namespace SocialNetwork.core.model.posts.application
+{
+    public class IntroductionRequestService
+    {
+        private IntroductionRequestRepository _introductionRequestRepository;
+        private ConnectionRequestRepository _connectionRequestRepository;
+
+        public IntroductionRequestService(SocialNetworkDbContext context)
+        {
+            _introductionRequestRepository = context.repositories().IntroductionRequest();
+            _connectionRequestRepository = context.repositories().ConnectionRequest();
+        }
+    }
+}
