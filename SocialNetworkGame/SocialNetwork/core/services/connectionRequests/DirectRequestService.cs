4a4cd6bb5da770c6bbd1dbb1f928a24ea1710c65
vieira420 <1190948@isep.ipp.pt>
2021-11-26 16:47:12 +0000

    OL41-2 #comment Resolução de erros no domain services do agregado ConnectionRequest

--- SocialNetworkGame/SocialNetwork/core/services/connectionRequests/DirectRequestService.cs
+++ SocialNetworkGame/SocialNetwork/core/services/connectionRequests/DirectRequestService.cs
@@ -6,6 +6,7 @@ using SocialNetwork.core.model.connectionRequests.repository;
 using SocialNetwork.core.model.players.domain;
 using SocialNetwork.core.model.relationships.domain;
 using SocialNetwork.core.model.shared;
+using SocialNetwork.core.model.tags.domain;
 
 namespace SocialNetwork.core.services.connectionRequests
 {
@@ -20,18 +21,18 @@ namespace SocialNetwork.core.services.connectionRequests
             _repo = repo;
         }
 
-        public async Task<List<DirectRequestDTO>> GetAllAsync()
+        public async Task<List<DirectRequestDto>> GetAllAsync()
         {
             var list = await _repo.GetAllAsync();
-            return list.ConvertAll<DirectRequestDTO>(directRequest => directRequest.ToDto());
+            return list.ConvertAll<DirectRequestDto>(directRequest => directRequest.ToDto());
         }
 
-        public async Task<DirectRequestDTO> AddAsync(CreateDirectRequestDto directRequestDto)
+        public async Task<DirectRequestDto> AddAsync(CreateDirectRequestDto directRequestDto)
         {
             DirectRequest directRequest = new(new PlayerId(directRequestDto.PlayerSender),
                 new PlayerId(directRequestDto.PlayerReceiver), TextBox.ValueOf(directRequestDto.Text),
-                ConnectionStrenght.ValueOf(directRequestDto.ConnectionStrength),
-                directRequestDto.Tags.ConvertAll(tags => Tag.ValueOf(tags)));
+                ConnectionStrength.ValueOf(directRequestDto.ConnectionStrength),
+                directRequestDto.Tags.ConvertAll(tags => new TagId(tags)));
 
             await _repo.AddAsync(directRequest);
             await _unitOfWork.CommitAsync();
