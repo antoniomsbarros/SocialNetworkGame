f6192f8cd626a437f0a1eb38942fb7c0788aa047
Daniel Reis <1200608@isep.ipp.pt>
2021-11-27 18:33:33 +0000

    OL41-13 Modificações no Value Object TagName

--- SocialNetworkGame/SocialNetwork/core/model/tags/domain/TagName.cs
+++ SocialNetworkGame/SocialNetwork/core/model/tags/domain/TagName.cs
@@ -1,17 +1,51 @@
 ﻿using SocialNetwork.core.model.shared;
+using System;
+using System.Text.RegularExpressions;
 
 namespace SocialNetwork.core.model.tags.domain
 {
-    public class TagName : Entity<TagId>
+    public class TagName : IValueObject
     {
+
+        private const string TagRegex = "^[a-zA-Z0-9 ]*$";
+
+        public string Name { get; }
+
+        protected TagName()
+        {
+            // for ORM
+        }
+
+        public TagName(string Name)
+        {
+            if (TagNameValid(Name))
+            {
+                this.Name = Name;
+            }
+            else if (TagNameValid(Name))
+                throw new BusinessRuleValidationException("Tag name invalid");
+        }
+
+        public static bool TagNameValid(string Name)
+        {
+            return new Regex(TagRegex).IsMatch(Name)
+                   && Name.Length > 0;
+        }
+
+        public static TagName ValueOf(string Name)
+        {
+            return new(Name);
+        }
+
         public override bool Equals(object obj)
         {
-            throw new System.NotImplementedException();
+            return obj is TagName name &&
+                   Name == name.Name;
         }
 
         public override int GetHashCode()
         {
-            throw new System.NotImplementedException();
+            return HashCode.Combine(Name);
         }
     }
 }
