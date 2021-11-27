d550d29cab867003dd8a4f919ce17d076028803d
Daniel Reis <1200608@isep.ipp.pt>
2021-11-27 18:24:27 +0000

    OL41-13  Criação do Tag Controller e Tag Service

---
+++ SocialNetworkGame/SocialNetwork/core/services/tags/TagsService.cs
@@ -0,0 +1,17 @@
+﻿using SocialNetwork.core.model.shared;
+using SocialNetwork.core.model.tags.repository;
+
+namespace SocialNetwork.core.services.tags
+{
+    public class TagsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ITagRepository _repo;
+
+        public TagsService(IUnitOfWork unitOfWork, ITagRepository repo)
+        {
+            _unitOfWork = unitOfWork;
+            _repo = repo;
+        }
+    }
+}
