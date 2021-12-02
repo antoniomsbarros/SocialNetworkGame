d684ef42aa683aebfc0161f495427069b347ed17
Luis Laranjeira <1191495@isep.ipp.pt>
2021-12-02 23:11:32 +0000

    OL41-30 # Adicionei a pesquisa do caminho mais curto

--- SocialNetworkGame/AI/ai_server.pl
+++ SocialNetworkGame/AI/ai_server.pl
@@ -6,11 +6,13 @@
 :- use_module(library(http/http_cors)).
 :- use_module(library(date)).
 :- use_module(library(random)).
+:- use_module(library(http/http_client)).
 
 % JSON Libraries
 :- use_module(library(http/json_convert)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/json)).
+:- use_module(library(http/http_open)).
 
 % Primary knowledge base
 
@@ -40,6 +42,8 @@ stopServer:-
 
 % HTTP Requests
 :- http_handler('/api/network/size', computeSocialNetworkSize, []).
+:- http_handler('/shortestPath', shortestPathRoute, []).
+
 
 % Methods
 
@@ -122,3 +126,55 @@ append_new([X|Y], Z, [X|W]):-
 append_new([_|Y], Z, W):-
     append_new(Y, Z, W).  
 
+
+%======== Shortest Path Between Two Users ========%
+
+
+% define shortest path route %
+shortestPathRoute(Request):-
+    cors_enable,
+    http_parameters(Request, [ userFrom(From, []), userDest(Dest, [])]),
+    to_lowercase(From, NormalizedUserFrom),
+    to_lowercase(Dest, NormalizedUserDest),
+    plan_shortestPath(NormalizedUserFrom, NormalizedUserDest, LCaminho_shortestway),
+    R = json(["Path"=LCaminho_shortestway]),
+    reply_json_dict(R).
+
+% conversion to lowercase %
+to_lowercase(User, UserNormalized):-
+    string_lower(User, Low),
+    normalize_space(atom(UserNormalized),Low).
+
+plan_shortestPath(Orig, Dest, LCaminho_shortestway) :-
+
+        (melhor_caminho_minimo(Orig, Dest);true),
+        retract(caminho_minimo(LCaminho_shortestway,_)),
+        nl.
+
+melhor_caminho_minimo(Orig, Dest):-
+        asserta(caminho_minimo(_,10000)),
+        dfs(Orig, Dest,LCaminho),
+        atualiza_melhor_caminho_minimo(LCaminho),
+        fail.
+
+atualiza_melhor_caminho_minimo(LCaminho):-
+        caminho_minimo(_,N),
+        length(LCaminho,C),
+        C<N,
+        retract(caminho_minimo(_,_)),
+        asserta(caminho_minimo(LCaminho,C)).
+
+
+dfs(Orig,Dest,Cam):-dfs2(Orig,Dest,[Orig],Cam).
+
+dfs2(Dest,Dest,LA,Cam):-!,
+        reverse(LA,Cam).
+
+dfs2(Act,Dest,LA,Cam):-
+        no(NAct,Act,_),
+        (ligacao(NAct,NX,_,_) ; ligacao(NX,NAct,_,_)),
+        no(NX,X,_),
+        \+ member(X,LA),
+        dfs2(X,Dest,[X|LA],Cam).
+
+%===================================================%
