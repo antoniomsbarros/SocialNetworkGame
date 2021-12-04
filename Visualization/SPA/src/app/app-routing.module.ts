e57576f4f3f6c479e64df3a507802979faadeb88
Antonio Barros (1200606) <1200606@isep.ipp.pt>
2021-12-04 11:59:56 +0000

    OL41-13 #comment adding the router in the header

--- Visualization/SPA/src/app/app-routing.module.ts
+++ Visualization/SPA/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import {HomeComponent} from './home/home.component';
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {LoginComponent} from "./login/login.component";
 import {GetListOfPendingConnectionRequestsComponent} from "./get-list-of-pending-connection-requests/get-list-of-pending-connection-requests.component";
+import {CreateIntroductionComponent} from "./create-introduction/create-introduction.component";
+import {ApproveDisapproveIntroductionRequestComponent} from "./approve-disapprove-introduction-request/approve-disapprove-introduction-request.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'signin', component: SignInComponent},
   {path: 'login', component: LoginComponent},
-  {path: "getlistPendig", component: GetListOfPendingConnectionRequestsComponent}
+  {path: "getlistPendig", component: GetListOfPendingConnectionRequestsComponent},
+  {path: "createIntroduction", component: CreateIntroductionComponent},
+  {path: "GetApprovolRequests", component: ApproveDisapproveIntroductionRequestComponent},
 ];
 
 @NgModule({
