90ea81f9133871b7fb4d158419d28885dae4a45e
Daniel Reis <1200608@isep.ipp.pt>
2021-12-05 18:53:03 +0000

    OL41-27 #comment  Predicados para obter utilizadores que tenham em comum X tags, em que X é parametrizável

--- SocialNetworkGame/AI/ai_server.pl
+++ SocialNetworkGame/AI/ai_server.pl
@@ -20,6 +20,9 @@
 
 % Secundary knowledge base
 :- dynamic relationship1/2.
+:- dynamic users_combination/2.
+:- dynamic best_solution_bidirectional/2.
+: -dynamic best_solution_unidirectional/2.
 
 % HTTP Server setup at 'Port'                           
 startServer(Port) :-   
@@ -44,7 +47,7 @@ stopServer:-
 :- http_handler('/api/network/size', computeSocialNetworkSize, []).
 :- http_handler('/shortestPath', shortestPathRoute, []).
 :- http_handler('/api/network/saferPath', saferPathRoute, []).
-
+:- http_handler('/api/tagsincommon', computePlayerWithXTagsInCommon, []).
 
 % Methods
 
@@ -182,35 +185,94 @@ dfs2(Act,Dest,LA,Cam):-
 
 %======== Strongest Path Between Two Users ========%
 
-stronger_path(Orig,Dest,LCaminho_forlig):-
+%===== Unidirectional ====%
+
+strongest_path_unidirectional(Orig,Dest,Result):-
     get_time(Ti),
-    (best_path(Orig,Dest);true),
-    retract(best_solution(LCaminho_forlig,S)),
+    (better_path_unidirectional(Orig,Dest);true),
+    retract(best_solution_unidirectional(Result,Sum)),
+    retract(counter(Counter)),
     get_time(Tf),
     T is Tf-Ti,
-    write('Time :'),write(T),nl,
-    write('Sum of Binding Forces :'),write(S),nl,
-    write('Solution :'),write(LCaminho_forlig),nl.
+    write('Number of Solutions :'),write(Counter),nl,
+    write('Solution generation time :'),write(T),nl,
+    write('Solution binding strength :'),write(Sum),nl,
+    write('Solution list :'),write(Result),nl.
 
-best_path(Orig,Dest):-
-    asserta(best_solution(_,0)),
-    dfs_force(Orig,Dest,LCaminho,S),
-    best_new_path(LCaminho,S),
+strongest_path_unidirectionalServer(Orig,Dest,Result):-
+    (better_path_unidirectional(Orig,Dest);true),
+    retract(best_solution_unidirectional(Result,Sum)),
+    retract(counter(Counter)).
+
+better_path_unidirectional(Orig,Dest):-
+    asserta(best_solution_unidirectional(_,0)),
+    asserta(counter(0)),
+    dfs_force_unidirectional(Orig,Dest,LCaminho,Sum),
+    update_better_unidirectional(LCaminho,Sum),
     fail.
 
-best_new_path(LCaminho,S):-
-    best_solution(_,N),
-    S>N,retract(best_solution(_,_)),
-    asserta(best_solution(LCaminho,S)).
+update_better_unidirectional(LCaminho,Sum):-
+    retract(counter(NS)),
+    NS1 is NS+1,
+    asserta(counter(NS1)),
 
-dfs_force(Orig,Dest,Cam,S):-
-    dfs_2_force(Orig,Dest,[Orig],Cam,S).
-
-dfs_2_force(Dest,Dest,LA,Cam,0):-!,reverse(LA,Cam).
-dfs_2_force(Act,Dest,LA,Cam,S):-no(NAct,Act,_),(ligacao(NAct,NX,S1,S2);ligacao(NX,NAct,S1,S2)),
-no(NX,X,_),\+ member(X,LA),dfs_2_force(X,Dest,[X|LA],Cam,SX),S is (SX+S1+S2) .
+    best_solution_unidirectional(_,N),
+    Sum>N,retract(best_solution_unidirectional(_,_)),
+    asserta(best_solution_unidirectional(LCaminho,Sum)).
 
 
+dfs_force_unidirectional(Orig,Dest,Cam,Sum):-
+    dfs2_force_unidirectional(Orig,Dest,[Orig],Cam,Sum).
+
+dfs2_force_unidirectional(Dest,Dest,LA,Cam,0):-!,reverse(LA,Cam).
+dfs2_force_unidirectional(Act,Dest,LA,Cam,Sum):-no(NAct,Act,_),(ligacao(NAct,NX,S1,_);ligacao(NX,NAct,_,S1)),
+no(NX,X,_),\+ member(X,LA),dfs2_force_unidirectional(X,Dest,[X|LA],Cam,SX),Sum is (SX+S1).
+
+%===== Bidirectional =====%
+
+strongest_path_bidirectional(Orig,Dest,Result):-
+    get_time(Ti),
+    (best_path_bidirectional(Orig,Dest);true),
+    retract(best_solution_bidirectional(Result,Sum)),
+    retract(counter(Count)),
+    get_time(Tf),
+    T is Tf-Ti,
+    write('Number of Solutions :'),write(Count),nl,
+    write('Solution generation time :'),write(T),nl,
+    write('Solution binding strength :'),write(Sum),nl,
+    write('Solution list :'),write(Result),nl.
+
+strongest_path_bidirectionalServer(Orig,Dest,Result):-
+    (best_path_bidirectional(Orig,Dest);true),
+    retract(best_solution_bidirectional(Result,Sum)),
+    retract(counter(Count)).
+
+best_path_bidirectional(Orig,Dest):-
+    asserta(best_solution_bidirectional(_,-10000)),
+    asserta(counter(0)),
+    dfs_force_bidirectional(Orig,Dest,LCaminho,Sum),
+    update_better_bidirectional(LCaminho,Sum),
+    fail.
+
+update_better_bidirectional(LCaminho,Sum):-
+
+retract(counter(NS)),
+NS1 is NS+1,
+asserta(counter(NS1)),
+
+    best_solution_bidirectional(_,N),
+    Sum>N,retract(best_solution_bidirectional(_,_)),
+    asserta(best_solution_bidirectional(LCaminho,Sum)).
+
+dfs_force_bidirectional(Orig,Dest,Cam,Sum):-
+dfs2_force_bidirectional(Orig,Dest,[Orig],Cam,Sum).
+
+dfs2_force_bidirectional(Dest,Dest,LA,Cam,0):-!,reverse(LA,Cam).
+dfs2_force_bidirectional(Act,Dest,LA,Cam,Sum):-no(NAct,Act,_),(ligacao(NAct,NX,S1,S2);ligacao(NX,NAct,S1,S2)),
+no(NX,X,_),\+ member(X,LA),dfs2_force_bidirectional(X,Dest,[X|LA],Cam,SX),Sum is (SX+S1+S2) .
+
+
+%===================================================%
 
 %======== Safer path route calculation =============%
 
@@ -287,3 +349,93 @@ dfs_safe_2(Act,Dest,LA,Cam,S,M):-
     S is (SX+S1+S2),
     M is min(MX, F).
 
+%======== User with X tags in common ========%
+
+computePlayerWithXTagsInCommon(Request) :-
+    getPlayerWithXTagsInCommon(Request, Result),
+    reply_json(Result).
+
+getPlayerWithXTagsInCommon(Request, Result) :-
+    http_parameters(Request, [combinations(X, [number])]),
+    getSocialNetworkHostPort(Host, Port),
+    http_open([host(Host), port(Port), path('/api/Players')], Stream, [cert_verify_hook(cert_accept_any)]),
+    json_read_dict(Stream, Data),
+    close(Stream),
+    createPlayersWithTags(Data),
+    find_users_with_X_common_tags(X, Result),
+    retractall(player_tags(_,_)). % Delete all terms generated from here
+
+createPlayersWithTags([PlayerX|OtherPlayers]):-
+    createPlayersWithTags(OtherPlayers),
+    asserta(player_tags(PlayerX.email, PlayerX.tags)).
+
+createPlayersWithTags([]).     
+
+%sinonimos("cinema","filme").
+%sinonimos("csharp","c#").
+%sinonimos("musica","cancao").
+
+find_users_with_X_common_tags(X,List_Result):-
+    all_tags(Todas_Tags),
+    findall(Combinacao,generate_combinations(X,Todas_Tags,Combinacao),Combinacoes),
+    findall(User,player_tags(User,_),Users),
+    commum_tags_combination(X,Users,Combinacoes),
+    findall([Comb,ListUsers],users_combination(Comb,ListUsers),List_Result),
+    retractall(users_combination(_,_)).
+
+commum_tags_combination(_,_,[]).
+commum_tags_combination(X,Users,[Combinacao|Combinacoes]):-
+    users_with_X_commun_tags2(X,Combinacao,Users,Users_Com_Tags),
+    commum_tags_combination(X,Users,Combinacoes),
+    !,
+    get_combination_synonyms(Combinacao, CombinacaoComSinonimos),
+    asserta(users_combination(CombinacaoComSinonimos,Users_Com_Tags)).
+
+users_with_X_commun_tags2(_,_,[],[]):-!.
+users_with_X_commun_tags2(X,Tags,[U|Users],Result):-
+    player_tags(U,User_Tags),
+    intersection_with_synonyms(Tags, User_Tags, Commun),
+    length(Commun, Size),
+    Size >= X, !,
+    users_with_X_commun_tags2(X,Tags,Users,Result1),
+    append([U], Result1, Result).
+users_with_X_commun_tags2(X,Tags,[_|Users],Result):-
+    !,
+    users_with_X_commun_tags2(X,Tags,Users,Result).
+
+
+get_tag_synonyms(Tag, LS2):-
+    findall(S, (sinonimos(Tag,S);sinonimos(S,Tag)), LS),
+    LS1 = [Tag|LS],
+    sort(LS1, LS2).
+
+
+get_combination_synonyms([], []):-!. 
+get_combination_synonyms([Tag|Tags], [LS|Combinacao]):-
+    get_combination_synonyms(Tags, Combinacao),
+    get_tag_synonyms(Tag, LS).
+
+
+intersection_with_synonyms([], _, []):-!.
+intersection_with_synonyms([Tag|Tags], User_Tags, [LC2|Commun]):-
+    intersection_with_synonyms(Tags, User_Tags, Commun),
+    get_tag_synonyms(Tag, LS),
+    intersection(LS, User_Tags, LC), LC\==[],
+    !,
+    LC1 = [Tag|LC],
+    sort(LC1, LC2).
+intersection_with_synonyms([_|Tags], User_Tags, Commun):-
+    intersection_with_synonyms(Tags, User_Tags, Commun).
+
+all_tags(Tags):-
+    findall(User_Tags,player_tags(_,User_Tags),Todas_Tags),
+    remove_equal_tags(Todas_Tags,Tags).
+
+remove_equal_tags([],[]).
+remove_equal_tags([Lista|Todas_Tags],Tags):-
+    remove_equal_tags(Todas_Tags,Tags1),!,
+    union(Lista,Tags1,Tags).
+
+generate_combinations(0,_,[]).
+generate_combinations(N,[X|T],[X|Comb]):-N>0,N1 is N-1,generate_combinations(N1,T,Comb).
+generate_combinations(N,[_|T],Comb):-N>0,generate_combinations(N,T,Comb).
