ab765d518167c10b9eb0e78bb7c354501fc6b526
Mykhaylo Kharlamov <1190906@isep.ipp.pt>
2021-12-05 14:41:25 +0000

    OL41-7 #comment Adicionei o onResize à componente da rede

--- Visualization/SPA/src/app/network/network.component.ts
+++ Visualization/SPA/src/app/network/network.component.ts
@@ -41,8 +41,15 @@ export class NetworkComponent implements OnInit {
   });
 
 
+  getTestNetwork() {
+    this.network = JSON.parse('{"playerId":"05d54773-07b4-4483-8653-0b2dbe112521","playerName":"Loy Seamus","playerTags":[],"relationships":[{"relationshipId":"0998e0ef-79e8-4195-a2ad-c35335e1124f","relationshipStrength":8,"relationshipTags":["t10"],"playerId":"1ae293fa-3e50-41e6-b87e-83ef4c36aac7","playerName":"Thaddeus Nico","playerTags":[],"relationships":[{"relationshipId":"cd2283f5-fc03-4aa9-952a-34748f5106bf","playerId":"8451f82d-943d-4338-a80e-3a2e73890358","playerName":"Harvey Alaina","relationships":[{"relationshipId":"032f6925-301c-4121-a59e-e3302549e174","playerId":"aa58b458-65c7-4941-b32a-62b06c24280d","playerName":"Courtney Ulises","relationships":[{"relationshipId":"b943cad5-c37b-4a2f-93b3-2b1896c6a1ee","playerId":"8451f82d-943d-4338-a80e-3a2e73890358","playerName":"Harvey Alaina","relationships":[]}]},{"relationshipId":"830cb59d-c7fd-4d33-8db7-16597b178c00","playerId":"e3aa14d3-c662-4e76-ba3b-3e1c40c3957c","playerName":"Magdalena Garnett","relationships":[]},{"relationshipId":"cdbc217c-f363-4b7c-ad96-e594e4cb77f8","playerId":"1ae293fa-3e50-41e6-b87e-83ef4c36aac7","playerName":"Thaddeus Nico","relationships":[]}]}]},{"relationshipId":"b0ec7397-352e-4bb5-a93c-0a59d985aab0","relationshipStrength":4,"relationshipTags":["t4"],"playerId":"e3aa14d3-c662-4e76-ba3b-3e1c40c3957c","playerName":"Magdalena Garnett","playerTags":[],"relationships":[{"relationshipId":"44a71855-9c28-4756-a776-8466f40d39a9","playerId":"8451f82d-943d-4338-a80e-3a2e73890358","playerName":"Harvey Alaina","relationships":[]}]}]}');
+
+
+  }
+
   getNetwork() {
     this.showDepthSelectionForm = false;
+    /*
     this.relationshipService.getNetworkFromPlayerByDepth("Elva33054057@gmail.com", this.networkDepth.value)
       .subscribe(data => {
         this.network = data;
@@ -50,6 +57,12 @@ export class NetworkComponent implements OnInit {
         this.initializeGraph();
         this.animate();
       })
+    */
+    this.getTestNetwork();
+    this.showNetworkGraph = true;
+    this.initializeGraph();
+    this.animate();
+
   }
 
 
@@ -73,7 +86,7 @@ export class NetworkComponent implements OnInit {
     this.camera = new THREE.OrthographicCamera(window.innerWidth / - 20, window.innerWidth / 20,
       window.innerHeight / 20, window.innerHeight / - 20, 1, 1000);
 
-    const maxDistanceY = 40, minDistanceY = -40, maxDistanceX = 40, minDistanceX = -40;
+    const maxDistanceY = 60, minDistanceY = -60, maxDistanceX = 60, minDistanceX = -60;
 
     //let distanceFromParentNodeRatio = 1;
     let nodes: any = {};
@@ -110,11 +123,12 @@ export class NetworkComponent implements OnInit {
     const circleGeometry = new THREE.CircleGeometry(4, 32);
 
     for (let i = 0; i < playerIds.length; i++) {
-      let material = new THREE.MeshBasicMaterial({ color: Math.floor(Math.random() * 0xffffff) });
+      let material = new THREE.MeshBasicMaterial({ color: 0x009EFA });
 
       let circle =
         i == 0
-        ? new THREE.Mesh(new THREE.CircleGeometry(6, 32), material)
+        ? new THREE.Mesh(new THREE.CircleGeometry(6, 32),
+            new THREE.MeshBasicMaterial({ color: 0xFF8066 }))
         : new THREE.Mesh(circleGeometry, material);
 
       circle.position.x = i == 0 ? 0 : Math.random() * (maxDistanceX - minDistanceX) + minDistanceX;
@@ -158,22 +172,26 @@ export class NetworkComponent implements OnInit {
     this.controls.enableZoom = true;
     this.controls.enablePan = true;
     this.controls.enableRotate = false;
-    this.controls.minZoom = 1;
+    this.controls.minZoom = 0.2;
     this.controls.maxZoom = 12;
+    this.controls.zoomSpeed = 2;
 
     this.camera.position.set(0, 0, 2);
 
     this.controls.update();
 
-    window.addEventListener('resize', this.onWindowResize, false);
+    window.addEventListener('resize', () => {
+      this.camera.left = window.innerWidth / - 20;
+      this.camera.right = window.innerWidth / 20;
+      this.camera.top = window.innerHeight / 20;
+      this.camera.bottom = window.innerHeight / -20;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }, false);
+
     window.document.body.style.overflow = "hidden";
   }
 
-  onWindowResize() {
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-  }
 
   animate() {
     requestAnimationFrame(this.animate.bind(this));
