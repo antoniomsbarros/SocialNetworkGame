d158cbc984aee72b244687949aa75e2545a092e7
vieira420 <1190948@isep.ipp.pt>
2021-12-16 11:23:00 +0000

    Correção de erros

--- Visualization/SPA/src/app/suggest-connection/suggest-connection.component.ts
+++ Visualization/SPA/src/app/suggest-connection/suggest-connection.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PlayersService} from "../services/players/players.service";
+import {PlayerDto} from "../dto/players/PlayerDto";
 
 @Component({
   selector: 'app-suggest-connection',
@@ -13,6 +14,10 @@ export class SuggestConnectionComponent implements OnInit {
 
   get connectionSuggestions() { // returns a list of suggestions for the user in the front-end
     return this.playerService.getConnectionSuggestion();
+  }
+
+  choosePlayer(player : PlayerDto) {
+    return this
   }
 
   ngOnInit(): void {
