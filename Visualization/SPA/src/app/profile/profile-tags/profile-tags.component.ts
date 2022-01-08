1824a08248111fd480c5a40d18a8ba60e18c0808
Daniel Reis <1200608@isep.ipp.pt>
2022-01-08 17:22:17 +0000

    OL41-74 #comment Implementação da Tag Cloud das Tags do proprio Utilizador

--- Visualization/SPA/src/app/profile/profile-tags/profile-tags.component.ts
+++ Visualization/SPA/src/app/profile/profile-tags/profile-tags.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {CloudData, CloudOptions, TagCloudModule} from 'angular-tag-cloud-module';
+import {firstValueFrom} from "rxjs";
+import {PlayerDto} from "../../dto/players/PlayerDto";
+import {PlayersService} from "../../services/players/players.service";
+import {prepareDataForTagCloud} from "./prepareDataForTagCloud";
+import {ActivatedRoute} from "@angular/router";
+
 
 @Component({
   selector: 'app-profile-tags',
@@ -6,10 +13,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-tags.component.css']
 })
 export class ProfileTagsComponent implements OnInit {
-
-  constructor() { }
+   userEmail!: any;
+  player!:PlayerDto;
+  constructor(private playerService: PlayersService,  private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getPLayerProfile();
+  }
+  options: CloudOptions = {
+    width: 1,
+    height: 200,
+    overflow: false,
+    zoomOnHover:{
+      scale:1.2,
+      transitionTime:0.3,
+      delay:0.3
+    },
+    realignOnResize:true
+  };
+
+  data: CloudData[] = [];
+  async getPLayerProfile() {
+    this.route.params.subscribe(params => {
+      this.userEmail = params['email'];
+    })
+    if (this.userEmail!=null){
+      this.player = await firstValueFrom(this.playerService.getProfile(this.userEmail));
+    this.data=prepareDataForTagCloud(this.player.tags)
+    }
   }
 
 }
